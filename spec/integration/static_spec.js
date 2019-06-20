9872ed044223bf7faf1e7fe1919728e4d29f7969
Meredith Harris <mlharris516@gmail.com>
2019-06-20 14:51:35 -0600

    added TDD GET /marco

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,6 +1,7 @@
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/";
+const marco = "http://localhost:3000/marco";
 
 describe("routes : static", () => {
 
@@ -11,6 +12,20 @@ describe("routes : static", () => {
             request.get(base, (err, res, body) => {
                 expect(res.statusCode).toBe(200);
 
+
+                done();
+            });
+        });
+    });
+
+
+    describe("GET /marco", () => {
+
+        it("should return status code 200, polo", (done) => {
+
+            request.get(marco, (err, res, body) => {
+                expect(res.statusCode).toBe(200);
+                expect(body).toBe("polo");
 
                 done();
             });
