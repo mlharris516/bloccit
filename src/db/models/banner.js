876aaba302640058157c18959cb69679cb47e09b
Meredith Harris <mlharris516@gmail.com>
2019-06-23 07:28:02 -0600

    completed topic resource 1 checkpoint

--- src/db/models/banner.js
+++ src/db/models/banner.js
@@ -3,15 +3,15 @@ module.exports = (sequelize, DataTypes) => {
   var Banner = sequelize.define('Banner', {
     source: DataTypes.STRING,
     description: DataTypes.STRING,
-   topicId: {
-     type: DataTypes.INTEGER,
-     onDelete: "CASCADE",
-     references: {
-       model: "Topic",
-       key: "id",
-       as: "topicId",
-     }
-   }
+    topicId: {
+      type: DataTypes.INTEGER,
+      onDelete: "CASCADE",
+      references: {
+        model: "Topics",
+        key: "id",
+        as: "topicId",
+      }
+    }
   }, {});
   Banner.associate = function(models) {
     // associations can be defined here
