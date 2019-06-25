60ffb9cae40466d0d484fc7c6e171622936149ed
Meredith Harris <mlharris516@gmail.com>
2019-06-25 09:12:39 -0600

    added edit/update feature

--- src/controllers/topicController.js
+++ src/controllers/topicController.js
@@ -50,6 +50,29 @@ module.exports = {
           res.redirect(303, "/topics")
         }
       });
+    },
+
+    edit(req, res, next) {
+      topicQueries.getTopic(req.params.id, (err, topic) => {
+        if(err || topic == null){
+          res.redirect(404, "/");
+        } else {
+          res.render("topics/edit", {topic});
+        }
+      });
+    },
+
+    update(req, res, next) {
+
+
+      topicQueries.updateTopic(req.params.id, req.body, (err, topic) => {
+
+        if(err || topic == null) {
+          res.redirect(404, `/topics/${req.params.id}/edit`);
+        } else {
+          res.redirect(`/toipcs/${topic.id}`);
+        }
+      });
     }
 
     
