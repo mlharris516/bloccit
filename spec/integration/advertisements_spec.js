1ef8cf0fd32da3e896dd391830c722201d97b2e7
Meredith Harris <mlharris516@gmail.com>
2019-06-30 08:58:42 -0600

    completed advertisement model - CRUD operations

--- spec/integration/advertisements_spec.js
+++ spec/integration/advertisements_spec.js
@@ -4,81 +4,169 @@ const base = "http://localhost:3000/advertisements/";
 const sequelize = require("../../src/db/models/index").sequelize;
 const Advertisement = require("../../src/db/models").Advertisement;
 
+
 describe("routes : advertisements", () => {
 
-    beforeEach((done) => {
-        this.advertisement;
-        sequelize.sync({force: true}).then((res) => {
+  beforeEach((done) => {
+    this.advertisement;
+    sequelize.sync({force: true}).then((res) => {
 
-            Advertisement.create({
-                title: "Advert 1",
-                description: "Buy this now"
-            })
-            .then((advertisement) => {
-                this.advertisement = advertisement;
-                done();
-            });
-
-        });
-
-    });
-
-    describe("GET /advertisements", () => {
-
-        it("should return a status code 200 and all advertisements", (done) => {
-            request.get(base, (err, res, body) => {
-                expect(res.statusCode).toBe(200);
-                expect(err).toBeNull();
-                expect(body).toContain("Advertisements");
-                expect(body).toContain("Advert 1");
-                done();
-            });
-        });
+        Advertisement.create({
+        title: "Grocery Store",
+        description: "There's one downtown"
+      })
+      .then((advertisement) => {
+        this.advertisement = advertisement;
+        done();
+      })
+      .catch((err) => {
+        console.log(err);
+        done();
+      });
 
     });
 
+  });
 
-    describe("GET /advertisements/new", () => {
+  describe("GET /advertisements", () => {
 
-        it("should render a new advertisement form", (done) => {
-            request.get(`${base}new`, (err, res, body) => {
-                expect(err).toBeNull();
-                expect(body).toContain("New Advertisement");
-                done();
-            });
-        });
-
+    it("should return a status code 200 and all advertisements", (done) => {
+      request.get(base, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        expect(err).toBeNull();
+        expect(body).toContain("Advertisements");
+        expect(body).toContain("Grocery Store");
+        done();
+      });
     });
 
-    describe("POST /advertisements/create", () => {
-        const options = {
-          url: `${base}create`,
-          form: {
-            title: "Trader Joe's",
-            description: "Apples: 10 for $1"
-          }
-        };
-  
-        it("should create a new topic and redirect", (done) => {
-  
-  //#1
-          request.post(options,
-  
-  //#2
-            (err, res, body) => {
-              Advertisement.findOne({where: {title: "Trader Joe's"}})
-              .then((advertisement) => {
-                expect(res.statusCode).toBe(303);
-                expect(advertisement.title).toBe("Trader Joe's");
-                expect(advertisement.description).toBe("Apples: 10 for $1");
-                done();
-              })
-              .catch((err) => {
-                console.log(err);
-                done();
-              });
-            }
-          );
+  });
+
+
+  describe("GET /advertisements/new", () => {
+
+    it("should render a new advertisement form", (done) => {
+      request.get(`${base}new`, (err, res, body) => {
+        expect(err).toBeNull();
+        expect(body).toContain("New Advertisement");
+        done();
+      });
+    });
+
+  });
+
+
+  describe("POST /advertisements/create", () => {
+    const options = {
+      url: `${base}create`,
+      form: {
+        title: "TJ's",
+        description: "There's one downtown"
+      }
+    };
+
+    it("should create a new advertisement and redirect", (done) => {
+
+//#1
+      request.post(options,
+
+//#2
+        (err, res, body) => {
+            Advertisement.findOne({where: {title: "TJ's"}})
+          .then((advertisement) => {
+            expect(res.statusCode).toBe(303);
+            expect(advertisement.title).toBe("TJ's");
+            expect(advertisement.description).toBe("There's one downtown");
+            done();
+          })
+          .catch((err) => {
+            console.log(err);
+            done();
+          });
+        }
+      );
+    });
+  });
+
+
+  describe("GET /advertisements/:id", () => {
+
+    it("should render a view with the selected advertisement", (done) => {
+      request.get(`${base}${this.advertisement.id}`, (err, res, body) => {
+        expect(err).toBeNull();
+        expect(body).toContain("Grocery Store");
+        done();
+      });
+    });
+
+  });
+
+
+  describe("POST /advertisements/:id/destroy", () => {
+    it("should delete the advertisement with the associated ID", (done) => {
+
+        Advertisement.all()
+      .then((advertisements) => {
+
+        const advertisementCountBeforeDelete = advertisements.length;
+
+        expect(advertisementCountBeforeDelete).toBe(1);
+
+        request.post(`${base}${this.advertisement.id}/destroy`, (err, res, body) => {
+            Advertisement.all()
+          .then((advertisements) => {
+            expect(err).toBeNull();
+            expect(advertisements.length).toBe(advertisementCountBeforeDelete - 1);
+            done();
+          })
+
         });
       });
+
+    });
+  
+  });
+
+
+  describe("GET /advertisements/:id/edit", () => {
+
+    it ("should render a view with an edit advertisement form", (done) => {
+      request.get(`${base}${this.advertisement.id}/edit`, (err, res, body) => {
+        expect(err).toBeNull();
+        expect(body).toContain("Edit Advertisement");
+        expect(body).toContain("Grocery Store");
+        done();
+      });
+    });
+
+  });
+
+
+  describe("POST /advertisements/:id/update", () => {
+
+    it("should update the advertisement with the given values", (done) => {
+      const options = {
+        url: `${base}${this.advertisement.id}/update`,
+        form: {
+          title: "Trader Joe's",
+          description: "There's one downtown"
+        }
+      };
+
+      request.post(options,
+        (err, res, body) => {
+
+          expect(err).toBeNull();
+
+          Advertisement.findOne({
+            where: { id: this.advertisement.id }
+          })
+          .then((advertisement) => {
+            expect(advertisement.title).toBe("Trader Joe's");
+            done();
+          });
+        });
+    });
+  });
+  
 });
