b0e0956033316385ec05ac8c9fdfa5ead9ab19f9
Meredith Harris <mlharris516@gmail.com>
2019-07-15 09:30:59 -0600

    completed validation checkpoint

--- spec/integration/posts_spec.js
+++ spec/integration/posts_spec.js
@@ -81,7 +81,32 @@ describe("routes : posts", () => {
              }
            );
          });
-     
+         
+         it("should not create a new post that fails validations", (done) => {
+             const options = {
+                 url: `${base}/${this.topic.id}/posts/create`,
+                 form: {
+                    title: "a",
+                    body: "b"
+                 }
+             };
+
+             request.post(options,
+                (err, res, body) => {
+
+                    Post.findOne({where: {title: "a"}})
+                    .then((post) => {
+                        expect(post).toBeNull();
+                        done();
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                        done();
+                    });
+                }
+            );
+         });
+         
       });
 
 
@@ -154,7 +179,8 @@ describe("routes : posts", () => {
             const options = {
               url: `${base}/${this.topic.id}/posts/${this.post.id}/update`,
               form: {
-                title: "Snowman Building Competition"
+                title: "Snowman Building Competition",
+                body: "I love watching them melt slowly."
               }
             };
             request.post(options,
@@ -173,5 +199,6 @@ describe("routes : posts", () => {
         });
    
       });
+
 
 });
