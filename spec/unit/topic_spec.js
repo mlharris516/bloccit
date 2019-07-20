5e3a639645f0cf3ef8829cacd704fbe095e42d7b
Meredith Harris <mlharris516@gmail.com>
2019-07-19 21:04:23 -0600

    completed authorization checkpoint

--- spec/unit/topic_spec.js
+++ spec/unit/topic_spec.js
@@ -1,38 +1,53 @@
 const sequelize = require("../../src/db/models/index").sequelize;
 const Topic = require("../../src/db/models").Topic;
 const Post = require("../../src/db/models").Post;
+const User = require("../../src/db/models").User;
 
 describe("POST", () => {
 
-    beforeEach((done) => {
-        this.topic;
-        this.post;
-        sequelize.sync({force:true}).then((res) => {
+    
+   beforeEach((done) => {
+    this.topic;
+    this.post;
+    this.user;
 
-            Topic.create({
-                title: "My Wonderful Bloccit Tests",
-                description: "I hope these work"
-            })
-            .then((topic) => {
-                this.topic = topic;
-                
-                Post.create({
-                    title: "The best post of all time",
-                    body: "This post is the best",
-                    topicId: this.topic.id
-                })
-                .then((post) => {
-                    this.post = post;
-                    done();
-                });
-            })
-            .catch((err) => {
-                console.log(err);
-                done();
-            });
-        });
+    sequelize.sync({force: true}).then((res) => {
+
+// #2
+      User.create({
+        email: "starman@tesla.com",
+        password: "Trekkie4lyfe"
+      })
+      .then((user) => {
+        this.user = user; //store the user
+
+// #3
+        Topic.create({
+          title: "Expeditions to Alpha Centauri",
+          description: "A compilation of reports from recent visits to the star system.",
+
+// #4
+          posts: [{
+            title: "My first visit to Proxima Centauri b",
+            body: "I saw some rocks.",
+            userId: this.user.id
+          }]
+        }, {
+
+// #5
+          include: {
+            model: Post,
+            as: "posts"
+          }
+        })
+        .then((topic) => {
+          this.topic = topic; //store the topic
+          this.post = topic.posts[0]; //store the post
+          done();
+        })
+      })
     });
-
+  });
     
     describe("#create()", () => {
         it("should create a topic object stored in database", (done) => {
@@ -67,7 +82,7 @@ describe("POST", () => {
         it("should return the associated posts", (done) => {
           this.topic.getPosts()
           .then((posts) => {
-            expect(posts[0].title).toBe("The best post of all time");
+            expect(posts[0].title).toBe("My first visit to Proxima Centauri b");
             done();
            });
          });
--- src/db/models/post.js
+++ src/db/models/post.js
@@ -14,6 +14,11 @@ module.exports = (sequelize, DataTypes) => {
     topicId: {
       type: DataTypes.INTEGER,
       allowNull: false
+    },
+
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
     }
     
   }, {});
@@ -22,6 +27,11 @@ module.exports = (sequelize, DataTypes) => {
 
     Post.belongsTo(models.Topic, {
       foreignKey: "topicId",
+      onDelete: "CASCADE"
+    });
+
+    Post.belongsTo(models.User, {
+      foreignKey: "userId",
       onDelete: "CASCADE"
     });
 
